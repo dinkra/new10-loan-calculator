4cf037a1e02763de4602aeef07221cb630d0a239
dinarakrasilnikova <dinarochka@gmail.com>
2019-06-25 13:23:21 +0200

    [Container] Calculator template

--- src/containers/LoanCalculator/LoanCalculator.js
+++ src/containers/LoanCalculator/LoanCalculator.js
@@ -1,11 +1,27 @@
 import React, { PureComponent } from 'react';
+import Box from 'components/Box';
 import Button from 'components/Button';
+import Slider from 'components/Slider';
 
 class LoanCalculator extends PureComponent {
   state = {};
 
   render() {
-    return <Button>Calculator</Button>;
+    return (
+      <Box>
+        <p>Please choose your product:</p>
+        <Button>Marketing</Button>
+        <Button>Equipment</Button>
+        <p>Please choose your legal form:</p>
+        <Button>BV</Button>
+        <Button>Eenmanszak</Button>
+        <p>How much do you want to borrow?</p>
+        <Slider />
+        <p>For how long?</p>
+        <Slider />
+        <p>Your interest rate is:</p>
+      </Box>
+    );
   }
 }
 
